6bc60c8fae2aedb828ce0706850d829a2c7db302
Jessica Jeya <jeyanthiran.jessica@gmail.com>
2022-10-27 16:38:14 -0400

    generate HTML file

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
-const profileDataArgs = process.argv.slice(2, process.argv.length);
-
-console.log(profileDataArgs);
-
+const fs = require('fs');
+const generatePage = require('./src/page-template.js');
 
 
-const printProfileData = profileDataArr => {
-    profileDataArr.forEach(profileItem => console.log(profileItem));
-  };
-  
-  printProfileData(profileDataArgs);
+const profileDataArgs = process.argv.slice(2);
+
+const [name, github] = profileDataArgs;
+
+
+fs.writeFile('index.html', generatePage(name,github), err => {
+  if (err) throw new Error(err);
+
+  console.log('Portfolio complete! Check out index.html to see the output!');
+});
