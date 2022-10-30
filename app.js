3f6b6f024aebc92f47d9a227d4a082ba61b7500f
Jessica Jeya <jeyanthiran.jessica@gmail.com>
2022-10-30 02:27:10 -0400

    condtional prompt for about section to be added

--- app.js
+++ app.js
@@ -31,9 +31,22 @@ return inquirer.prompt([
       }
     },
     {
+      type: 'confirm',
+      name: 'confirmAbout',
+      message: 'Would you like to enter some information about yourself for an "About" section?',
+      default: true
+    },
+    {
       type: 'input',
       name: 'about',
-      message: 'Provide some information about yourself:'
+      message: 'Provide some information about yourself:',
+      when: ({ confirmAbout }) => {
+        if (confirmAbout) {
+          return true;
+        } else {
+          return false;
+        }
+      }
     }
   ]);
 };
