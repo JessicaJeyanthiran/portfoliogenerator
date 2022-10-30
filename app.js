f2ccdd1671205ce10a7dc3cf786e0116547b637f
Jessica Jeya <jeyanthiran.jessica@gmail.com>
2022-10-29 22:40:39 -0400

    installing inquirer

--- app.js
+++ app.js
@@ -1,14 +1,23 @@
+const inquirer = require('inquirer');
+
+inquirer
+  .prompt([
+    {
+      type: 'input',
+      name: 'name',
+      message: 'What is your name?'
+    }
+  ])
+  .then(answers => console.log(answers));
+
 const fs = require('fs');
 const generatePage = require('./src/page-template.js');
 
 
-const profileDataArgs = process.argv.slice(2);
+// const pageHTML = generatePage(name, github);
 
-const [name, github] = profileDataArgs;
+// fs.writeFile('./index.html', pageHTML, err => {
+//   if (err) throw err;
 
-
-fs.writeFile('index.html', generatePage(name,github), err => {
-  if (err) throw new Error(err);
-
-  console.log('Portfolio complete! Check out index.html to see the output!');
-});
+//   console.log('Portfolio complete! Check out index.html to see the output!');
+// });
