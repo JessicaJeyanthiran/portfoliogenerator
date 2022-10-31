4fe0485b5a854ca978b20fde5dc04265e14cccc2
Jessica Jeya <jeyanthiran.jessica@gmail.com>
2022-10-31 13:32:27 -0400

    created a function to include conditional about section

--- src/page-template.js
+++ src/page-template.js
@@ -1,3 +1,18 @@
+// create the about section
+const generateAbout = aboutText => {
+    if (!aboutText) {
+      return '';
+    }
+  
+    return `
+      <section class="my-3" id="about">
+        <h2 class="text-dark bg-primary p-2 display-inline-block">About Me</h2>
+        <p>${aboutText}</p>
+      </section>
+    `;
+  };
+
+
 module.exports = templateData => {
     // destructure projects and about data from templateData based on their property key names
     const { projects, about, ...header } = templateData;
@@ -28,7 +43,7 @@ module.exports = templateData => {
             </div>
         </header>
         <main class="container my-5">
-
+            ${generateAbout(about)}
         </main>
         <footer class="container text-center py-3">
             <h3 class="text-dark">&copy; ${new Date().getFullYear()} by ${header.name}</h3>
